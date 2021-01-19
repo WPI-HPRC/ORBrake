b28f67c22e741e74ef3d131956c41adb5ac59eda
Max Schrader <cmschrader@wpi.edu>
2021-01-18 21:48:28 -0500

    Build file working with tests

--- src/net/sf/openrocket/ORBrake/ORBrakeTest.java
+++ src/net/sf/openrocket/ORBrake/ORBrakeTest.java
@@ -1,38 +1,40 @@
-//package net.sf.openrocket.ORBrake;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//class ORBrakeTest {
-//	
-//	static ORBrakeSimulationListener listener;
-//
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//		listener = new ORBrakeSimulationListener();
+package net.sf.openrocket.ORBrake;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ORBrakeTest {
+	
+	static ORBrakeSimulationListener listener;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		listener = new ORBrakeSimulationListener();
+	}
+
+//	@AfterAll
+//	static void tearDownAfterClass() throws Exception {
 //	}
-//
-////	@AfterAll
-////	static void tearDownAfterClass() throws Exception {
-////	}
-//
-//	@BeforeEach
-//	void setUp() throws Exception {
-//		listener.velocity = 300;
-//		listener.altitude = 3000;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		listener.velocity = 300;
+		listener.altitude = 3000;
+	}
+
+//	@AfterEach
+//	void tearDown() throws Exception {
 //	}
-//
-////	@AfterEach
-////	void tearDown() throws Exception {
-////	}
-//
-//	@Test
-//	void dragSurfaceTest() {
-//		assertEquals(listener.dragSurface(5), 5555, .0001);
-//	}
-//}
+
+	@Test
+	void dragSurfaceTest() {
+		double drag = listener.dragSurface(5);
+		System.out.println(drag);
+		assertEquals(drag, 4.8284, .0001);
+	}
+}
